ce06de021f3e90a46c80f2cf828ac87c50f6bc21
Venushka Perera <venushka.perera@alfasystems.com>
2017-02-04 18:01:44 +0000

    Making the download URL customisable to allow using an alternate download URL.

--- manifests/install.pp
+++ manifests/install.pp
@@ -47,7 +47,7 @@ class crucible::install {
 
   # Download and install the crucible directory if version file doesn't exist
   exec { 'install_crucible':
-    command => "/usr/bin/wget -q -O /tmp/crucible-${crucible::version}.zip https://www.atlassian.com/software/crucible/downloads/binary/crucible-${crucible::version}.zip && /usr/bin/unzip /tmp/crucible-${crucible::version}.zip -d /tmp/ && mv /tmp/fecru-${crucible::version} ${crucible::install_dir}-${crucible::version} && chown -R ${crucible::service_user}.${crucible::service_user} ${crucible::install_dir}-${crucible::version}",
+    command => "/usr/bin/wget -q -O /tmp/crucible-${crucible::version}.zip ${crucible::download_url}/crucible-${crucible::version}.zip && /usr/bin/unzip /tmp/crucible-${crucible::version}.zip -d /tmp/ && mv /tmp/fecru-${crucible::version} ${crucible::install_dir}-${crucible::version} && chown -R ${crucible::service_user}.${crucible::service_user} ${crucible::install_dir}-${crucible::version}",
     creates => "${crucible::install_dir}-${crucible::version}",
     require => User[$crucible::service_user],
   }
